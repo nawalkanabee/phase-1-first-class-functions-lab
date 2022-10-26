87a097949a51cc75e9d24f79352c1046013ffb51
nawalkanabee <nawalkanabee@gmail.com>
2022-10-26 20:56:23 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = (array) => {
+  return array.slice(0, 1);
+};
+console.log(returnFirstTwoDrivers(drivers));
+const returnLastTwoDrivers = (array) => {
+  return array.slice(-2);
+};
+console.log(returnLastTwoDrivers(drivers));
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers[0], drivers);
+
+
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler);
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler);
+
+const selectDifferentDrivers = function (arr, x) {
+  return x(arr);
+};
